6740bb16358cbb9d175caf0a6f4094ab520bcc5f
Luiz Luz <ziulluz@gmail.com>
2023-06-24 10:04:45 -0300

    Refac distance map func, for modularity

--- generate_distance_map.py
+++ generate_distance_map.py
@@ -3,86 +3,92 @@ import os
 import numpy as np
 
 from osgeo import gdal
-from osgeo import gdal_array
-from osgeo import osr
+
 from scipy.ndimage import distance_transform_edt
 from scipy.ndimage import gaussian_filter
 from skimage.measure import label
+
 from src.utils import check_folder, array2raster, read_tiff, read_yaml, print_sucess
-import pandas as pd
-import matplotlib.pyplot as plt
-
-args = read_yaml("args.yaml")
 
 
-def apply_gaussian_distance_map(input_file, output_file):
-    """Apply distance map with a gaussian filter to the ground truth segmentantion
-    and save it as a tiff file
+
+
+def apply_gaussian_distance_map(input_img:np.array)->np.array:
+    """Apply euclidean distance transform and gaussian filter to the input image
 
     Parameters
     ----------
-    input_file : str
-        File name of the input image in the DATA_PATH folder
-    output_file : str
-        Output file name to be saved in the DATA_PATH folder
-    """
-    DATA_PATH = args.data_path
-    input_image_path = os.path.join(DATA_PATH, input_file)
-    
-    reference_img = gdal.Open(input_image_path)
-
-    ref = read_tiff(input_image_path).astype('uint16')
-    ref[ref>0] = 1
-    # generate the distance map
-    label_ref = label(ref)
-    new_lab = label_ref.copy()
-    new_lab = distance_transform_edt(new_lab)
-    new_lab = gaussian_filter(new_lab, sigma=5)
-    save_lab = np.zeros(label_ref.shape)
-    for obj in np.unique(label_ref)[1:]:
-        try:
-            save_lab[label_ref==obj] = new_lab[label_ref==obj]/np.max(new_lab[label_ref==obj])
-        except:
-            print('what')
-    
-    # save the distance map
-    output_image_path = os.path.join(DATA_PATH, output_file)
-    array2raster(output_image_path, reference_img, save_lab, 'Float32')
-
-
-
-#%%
-
-
-
-def is_generated(output_file):
-    """Check if the distance map has already been generated
+    input_img : np.array
+        Input image matrix with the segmentation
 
     Returns
     -------
-    bool
-        True if the distance map has already been generated, False otherwise
+    np.array
+        Output image matrix with the distance map and gaussian filter applied to each segmentation component
     """
-    if os.path.exists(os.path.join(args.data_path, "before_iter")):
-        return os.path.exists(os.path.join(args.data_path, "before_iter", output_file))
-        
-    else:
-        return False
 
+    ref = input_img.copy()
+    ref[ref>0] = 1
+
+    # label the image as components
+    label_ref = label(ref)
+
+    # apply distance transform and gaussian filter
+    new_lab = label_ref.copy()
+    new_lab = distance_transform_edt(new_lab)
+    new_lab = gaussian_filter(new_lab, sigma=5)
+    
+
+    # create the new image with the distance map
+    save_lab = np.zeros(label_ref.shape)
+
+    for obj in np.unique(label_ref)[1:]:
+        # normalize the distance map
+        save_lab[label_ref==obj] = new_lab[label_ref==obj]/np.max(new_lab[label_ref==obj])
+        
+    return save_lab
+    
 
 
 
 # %%
-def generate_distance_map(input_image, output_image):
-    if not is_generated(output_image):
-        check_folder(os.path.join(args.data_path, "before_iter"))
-        output_path = os.path.join(args.data_path, "before_iter", output_image)
-        input_path = os.path.join(args.data_path,"segmentation", input_image)
-        apply_gaussian_distance_map(input_path, output_path)
-        
+def generate_distance_map(input_image_path:str, output_image_path:str):
+    """Generate the distance map from the input image and save it in the output path
+    
+    Parameters
+    ----------
+    input_image_path : str
+        Path to the input image
+    
+    output_image_path : str
+        Path to save the output image
+    """
+    
+    if not os.path.exists(input_image_path):
+        raise FileNotFoundError("Input image not found")
 
-def generate_train_test_map():
-    generate_distance_map(args.train_segmentation_file, "train_distance_map.tif")
-    generate_distance_map(args.test_segmentation_file, "test_distance_map.tif")
+    if not os.path.exists(os.path.dirname(output_image_path)):
+        raise FileNotFoundError("Output folder not found")
+
+    img_lazy_load = gdal.Open(input_image_path)
+
+    input_img = read_tiff(input_image_path).astype('uint16')
+    
+    output_img = apply_gaussian_distance_map(input_img)
+
+    array2raster(output_image_path, img_lazy_load, output_img, 'Float32')
+
+
+if __name__ == "__main__":
+    
+    args = read_yaml("args.yaml")
+    
+    train_input_path = os.path.join(args.data_path, args.train_segmentation_file)
+
+    train_output_path = os.path.join(args.data_path, "test", "train_distance_map.tif")
+    
+    check_folder(os.path.dirname(train_output_path))
+
+    generate_distance_map(train_input_path, train_output_path)
+    
     print_sucess("Distance map generated successfully")
-
