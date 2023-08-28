c406f97bea0b51d5f9a5b63916452a30dc6306ee
Luiz Luz <ziulluz@gmail.com>
2023-08-28 09:04:00 -0300

    New stopping condition

--- main.py
+++ main.py
@@ -586,6 +586,10 @@ while current_iter < 10:
     # get current iteration folder
     current_iter_folder = get_current_iter_folder(args.data_path, args.test_itc, args.overlap)
     current_iter = int(current_iter_folder.split("_")[-1])
+
+    if current_iter > 5:
+        break
+    
     print("Current iteration folder: ", current_iter_folder)
     
     if current_iter == 0:
