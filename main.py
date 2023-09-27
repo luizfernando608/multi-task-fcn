de28d1c116b44be7ddb2e442673d0c08ad754578
Luiz Luz <ziulluz@gmail.com>
2023-09-27 19:55:20 -0300

    Add dropout func in the main pipeline

--- main.py
+++ main.py
@@ -440,7 +440,8 @@ def train_iteration(current_iter_folder:str, args:dict):
         args.nb_class,  
         args.arch, 
         args.filters, 
-        args.is_pretrained)
+        args.is_pretrained,
+        args.dropout_prob)
 
 
     ########## LOAD MODEL WEIGHTS FROM THE LAST CHECKPOINT ##########
