b182942000d8a3dc943434c36cd7c2eb88179f88
Luiz Luz <ziulluz@gmail.com>
2023-10-12 13:28:13 -0300

    Array2raster func with rasterio

--- src/utils.py
+++ src/utils.py
@@ -44,60 +44,66 @@ TRUTHY_STRINGS = {"on", "true", "1"}
 def get_device():
     return torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-def array2raster(newRasterfn:str, dataset:gdal.Dataset, array:np.ndarray, dtype:str):
-    """Save GTiff file from numpy.array
+
+
+def array2raster(path_to_save:str, array:np.ndarray, image_metadata:dict, dtype:str):
+    """Save a NumPy array as a GeoTIFF file.
 
     Parameters
     ----------
-    newRasterfn : string
-        File path to save .tif
-    dataset : 
-        Dataset from gdal.Open()
-    array : numpy.ndarray
-        Array image to save as .tif
-    dtype : string
-        Data Type to save image
-        Options: "Byte" or "Float32"
+    path_to_save : str
+        The file path to save the array as a GeoTIFF file.
+    array : np.ndarray
+        The image array or tensor with the format `(band, height, width)` or `(height, width)`.
+    image_metadata : dict
+        Image metadata obtained from the `get_image_metadata` function.
+    dtype : str
+        Data type for the output GeoTIFF:
+        - None: Use the same data type as specified in `image_metadata`.
+        - 'byte': Use the same data type as in the input NumPy array.
+        - Any other dtype compatible with the rasterio library.
     """
-    cols = array.shape[1]
-    rows = array.shape[0]
-    originX, pixelWidth, b, originY, d, pixelHeight = dataset.GetGeoTransform() 
-
-    driver = gdal.GetDriverByName('GTiff')
 
     # set data type to save.
-    GDT_dtype = gdal.GDT_Unknown
-    if dtype == "Byte": 
-        GDT_dtype = gdal.GDT_Byte
-    elif dtype == "Float32":
-        GDT_dtype = gdal.GDT_Float32
+    if dtype == None:
+        RASTER_DTYPE = meta.dtype
+    
+    elif dtype.lower() == "byte": 
+        RASTER_DTYPE = array.dtype
+
     else:
-        print("Not supported data type.")
+        RASTER_DTYPE = dtype.lower()
+
 
     # set number of band.
     if array.ndim == 2:
-        band_num = 1
+        BAND_NUM = 1
+        HEIGHT = array.shape[0]
+        WIDTH = array.shape[1]
+
     else:
-        band_num = array.shape[2]
+        BAND_NUM = array.shape[0]
+        HEIGHT = array.shape[1]
+        WIDTH  = array.shape[2]
 
-    outRaster = driver.Create(newRasterfn, cols, rows, band_num, GDT_dtype)
-    outRaster.SetGeoTransform((originX, pixelWidth, 0, originY, 0, pixelHeight))
 
-    # Loop over all bands.
-    for b in range(band_num):
-        outband = outRaster.GetRasterBand(b + 1)
-        # Read in the band's data into the third dimension of our array
-        if band_num == 1:
-            outband.WriteArray(array)
-        else:
-            outband.WriteArray(array[:,:,b])
+    with rasterio.open(
+        fp = path_to_save,
+        mode = "w",
+        driver = image_metadata['driver'],
+        height = HEIGHT,
+        width = WIDTH,
+        count = BAND_NUM,
+        dtype = RASTER_DTYPE,
+        crs = image_metadata['crs'],
+        transform = image_metadata['transform']
+    ) as writer:
+        
+        for band in range(1, BAND_NUM + 1):
 
-    # setteing srs from input tif file.
-    prj=dataset.GetProjection()
-    outRasterSRS = osr.SpatialReference(wkt=prj)
-    outRaster.SetProjection(outRasterSRS.ExportToWkt())
-    outband.FlushCache()
-
+            writer.write(array[band - 1, :, :], band)
+        
+    
 
 
 def add_padding_new(img:np.ndarray, psize:int, overl:float, const:int = 0) -> Tuple:
