5cec8d1e2e1f765e1e7cc73fdf92c74f379c36cb
luizfernando608 <ziulluz@gmail.com>
2023-10-25 20:58:50 -0300

    Residual Block

--- src/deepvlab3plus_resnet9.py
+++ src/deepvlab3plus_resnet9.py
@@ -4,6 +4,94 @@ import torch.nn.functional as F
 import numpy as np
 
 
+
+def conv_padding_same(kernel_size , **kwargs):
+    
+    padding = (kernel_size - 1) // 2
+
+    return nn.Conv2d(**kwargs, padding = padding, kernel_size = kernel_size)
+
+
+class ResidualBlock(nn.Module):
+    def __init__(
+        self,
+        input_channels: int,
+        output_channels: int,
+        downsample: bool,
+    ) -> None:
+        super(ResidualBlock, self).__init__()
+        
+        if input_channels != output_channels:
+            raise ValueError("Invalid configuration: The number of input channels ({}) should match the number of output channels ({}).".format(input_channels, output_channels))
+
+        
+        self.downsample = downsample
+
+        self.batch_norm1 = nn.BatchNorm2d(
+            num_features=input_channels,
+            momentum=0.9,
+            eps=1e-05,
+            affine=True,
+            track_running_stats=True,
+        )
+        self.elu = nn.ELU()
+
+        if downsample:
+  
+            self.conv_down1 = conv_padding_same(
+                in_channels=input_channels,
+                out_channels=output_channels,
+                kernel_size=3,
+                stride=2
+            )
+
+            self.conv_down2 = conv_padding_same(
+                in_channels=output_channels,
+                out_channels=output_channels,
+                kernel_size=1,
+                stride=2,
+            )
+            input_channels = output_channels
+
+
+        self.conv1 = conv_padding_same(
+            in_channels=input_channels,
+            out_channels=output_channels,
+            kernel_size=3,
+            stride=1,
+        )
+
+        self.batch_norm2 = nn.BatchNorm2d(
+            num_features=input_channels,
+            momentum=0.9,
+            eps=1e-05,
+            affine=True,
+            track_running_stats=True,
+        )
+
+        self.conv2 = conv_padding_same(
+            in_channels=input_channels,
+            out_channels=output_channels,
+            kernel_size=3,
+            stride=1,
+        )
+
+    def forward(self, x):
+        x_init = x
+
+        x = self.batch_norm1(x)
+        x = self.elu(x)
+
+        if self.downsample:
+            x = self.conv_down1(x)
+            x_init = self.conv_down2(x_init)
+
+        x = self.batch_norm2(x)
+        x = self.conv2(x)
+
+        return x + x_init
+
+
 class DeepLabv3Plus_resnet9(nn.Module):
     def __init__(self, num_ch, psize, num_class):
         super(DeepLabv3Plus_resnet9, self).__init__()
