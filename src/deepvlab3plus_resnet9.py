c578f5d1b948c4de5549e8e63ec37c800f0869fa
luizfernando608 <ziulluz@gmail.com>
2023-10-25 21:53:48 -0300

    Encoder model for patch size 128

--- src/deepvlab3plus_resnet9.py
+++ src/deepvlab3plus_resnet9.py
@@ -93,51 +93,95 @@ class ResidualBlock(nn.Module):
 class DeepLabv3Plus_resnet9(nn.Module):
     def __init__(self, num_ch, psize, num_class):
         super(DeepLabv3Plus_resnet9, self).__init__()
-        
-        self.num_ch_1 = num_ch
+
+        self.num_ch = num_ch
         self.psize = psize
         self.nb_class = num_class
 
+        self.conv1 = conv_padding_same(in_channels = self.num_ch,
+                                       out_channels = 64,
+                                       kernel_size = 3, 
+                                       stride = 1)
+        
+        self.res_block1 = ResidualBlock(in_channels=64,
+                                        out_channels=64,
+                                        downsample=False)
+        
+        self.res_block2 = ResidualBlock(in_channels=64,
+                                        out_channels=128,
+                                        downsample=True)
+        
+        self.res_block3 = ResidualBlock(in_channels=128,
+                                        out_channels=128,
+                                        downsample=False)
+    
+        self.res_block3 = ResidualBlock(in_channels=128,
+                                        out_channels=256,
+                                        downsample=True)
+        
+        self.res_block4 = ResidualBlock(in_channels=256,
+                                        out_channels=256,
+                                        downsample=False)
+
+        self.batch_norm1 = nn.BatchNorm2d(num_features=256, momentum=0.9, eps=1e-05, affine=True, track_running_stats=True)
+
+        self.elu1 = nn.ELU()
+
     def enconder(self, x):
-        pass
+        x = self.conv1(x)
+        x = self.res_block1(x)
+        x = self.res_block2(x)
+        x = self.res_block3(x)
+        
+        x_skip = x
+
+        x = self.res_block4(x)
+        x = self.batch_norm1(x)
+        x = self.elu1(x)
+        
+        
+        return x, x_skip
 
     def decoder_class(self, x):
         pass
-    
+
     def decoder_aux(self, x):
         pass
-    
+
     def forward(self, x):
         pass
 
 
 
 if __name__ == "__main__":
-    image = torch.randn(1, 64, 128, 128)
-
-    model = ResidualBlock(in_channels=64, out_channels=64, downsample=False)
-
-    with torch.no_grad():
-        output = model(image)
-
-    print(output.size())
-
-    model = ResidualBlock(in_channels = 64, out_channels=64, downsample=True)
-
-    with torch.no_grad():
-        output = model(image)
-    
-    print(output.size())
-    # model = DeepLabv3Plus_resnet9(10,
-    #              num_ch_1 = 25,
-    #              psize = 128,
-    #              nb_class = 14)
-
-    # model.eval()
-
     # image = torch.randn(1, 25, 128, 128)
 
+    # model = ResidualBlock(in_channels=64, out_channels=64, downsample=False)
+
     # with torch.no_grad():
-    #     output_cl, output_depth = model.forward(image)
-    # print(output_cl.size())
-    # print(output_depth.size())
+    #     output = model(image)
+
+    # print(output.size())
+
+    # model = ResidualBlock(in_channels = 64, out_channels=64, downsample=True)
+
+    # with torch.no_grad():
+    #     output = model(image)
+    
+    # print(output.size())
+
+    model = DeepLabv3Plus_resnet9(
+        num_ch=25,
+        num_class=14,
+        psize = 128,
+    )
+
+    model.eval()
+
+    image = torch.randn(1, 25, 128, 128)
+
+    with torch.no_grad():
+        x, x = model.enconder(image)
+    
+    print(x.size())
+    print(x.size())
