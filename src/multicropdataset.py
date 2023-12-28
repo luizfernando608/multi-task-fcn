3eceb9dd97cb6471a6331e0bb9477002bbecee14
luizfernando608 <ziulluz@gmail.com>
2023-12-28 10:36:07 -0300

    Dataset with lazy loading image patch from tiff file

--- src/multicropdataset.py
+++ src/multicropdataset.py
@@ -187,36 +187,248 @@ class DatasetFromCoord(Dataset):
         return image
 
 
-if __name__ == "__main__":
 
-    import sys
-    sys.path.append("/home/luiz/multi-task-fcn")
+def oversample(coords: np.ndarray, 
+               coords_label: np.ndarray, 
+               method: Literal["max", "median"]) -> np.ndarray:
+    """Oversamples data to balance classes based on segmentation samples.
 
-    from model import define_loader
-    from utils import oversamp, read_tiff, read_yaml
+    Parameters
+    ----------
+    coords : np.ndarray
+        Coordinates of the segmentation samples where non-zero values exist.
+    coords_label : np.ndarray
+        Segmentation labels corresponding to non-zero values. Each value relates to the pixel label at the `coords` position.
+    method : Literal["max", "median"]
+        The oversampling method to balance the tree-type classes.
+        - "max" for maximizing class counts.
+        - "median" for achieving class counts closer to the median.
 
-    args = read_yaml("args.yaml")
+    Returns
+    -------
+    np.ndarray
+        Balanced segmentation sample coordinates with non-zero values.
+        The coordinates are adjusted to balance tree-type classes based on the chosen oversampling method.
+    """
 
-    current_iter_folder = "/home/luiz/multi-task-fcn/1th_version_data/iter_001"
-    current_iter = int(current_iter_folder.split("_")[-1])
-    DATA_PATH = "/home/luiz/multi-task-fcn/1th_version_data"  
-
-    ######### Define Loader ############
-    LABEL_PATH = os.path.join(DATA_PATH, "segmentation","samples_A1_train2tif.tif" )
-    raster_train = read_tiff(LABEL_PATH)
-
-    DEPTH_PATH = os.path.join(DATA_PATH, "iter_000", "distance_map", "train_distance_map.tif")
-    depth_img = read_tiff(DEPTH_PATH)
-
-    image, coords_train, raster_train, labs_coords_train = define_loader(args.ortho_image, 
-                                                                         raster_train, 
-                                                                         args.size_crops)
     
-    ######## do oversampling in minor classes
-    coords_train = oversamp(coords_train, labs_coords_train, under=False)
+    uniq, count = np.unique(coords_label, return_counts=True)
+    
+    if method == "max":
+        upper_samp_limit = np.max(count)
+    
+    elif method == "median":
+        upper_samp_limit = int(np.median(count))
+    
+    else:
+        raise NotImplementedError(f"Method ``{method}`` were not implemented yet.")
 
-    if args.samples > coords_train.shape[0]:
-        args.samples = None
+    out_coords = np.zeros( (upper_samp_limit*len(uniq), 2), dtype='int64')
+    
+
+    for j in range(len(uniq)):
+
+        lab_ind = np.where(coords_label == uniq[j]) 
+
+        # If num of samples where the class is present is less than max_samp
+        # then we need to oversample
+        if len(lab_ind[0]) < upper_samp_limit:
+            # Randomly select samples with replacement to match max_samp
+            index = np.random.choice(lab_ind[0], upper_samp_limit, replace=True)
+            # Add to output array
+            out_coords[j*upper_samp_limit:(j+1)*upper_samp_limit,:] = coords[index]
+            
+        # If the number of samples where the class is present is the same as max_samp
+        # then we don't need to oversample, just add the samples randomly to the output array
+        else:
+            # Randomly select samples without replacement
+            index = np.random.choice(lab_ind[0], upper_samp_limit, replace=False)
+            # Add to output array
+            out_coords[j*upper_samp_limit:(j+1)*upper_samp_limit,:] = coords[index]
+    
+    # shuffle out_coords order
+    np.random.shuffle(out_coords)
+
+    return out_coords
+
+
+
+class DataSetFromImagePath(Dataset):
+    def __init__(self,
+                 image_path:str,
+                 segmentation_path:str,
+                 distance_map_path:str,
+                 samples:int,
+                 crop_size:int,
+                 dataset_type:Literal["train", "val", "test"],
+                 augment:bool,
+                 overlap_rate = None
+                 ) -> None:
+        
+        super().__init__()
+
+        if (dataset_type == "test") and (overlap_rate == None):
+            raise ValueError("Provide 'overlap_rate' for 'test' dataset_type")
+
+        self.image_path = image_path
+        self.segmentation_path = segmentation_path
+        self.distance_map_path = distance_map_path
+        
+        self.samples = samples
+        self.crop_size = crop_size
+        self.dataset_type = dataset_type
+        self.augment = augment
+
+        self.overlap_rate = overlap_rate
+
+        self.img_segmentation = read_tiff(segmentation_path)
+
+        self.generate_coords()
+
+
+
+    
+    def generate_coords(self):
+
+        if self.dataset_type == "train":
+            
+            coords = np.where(self.img_segmentation!=0)
+            coords = np.array(coords)
+            coords = np.rollaxis(coords, 1, 0)
+            
+            coords_label = self.img_segmentation[np.nonzero(self.img_segmentation)]
+
+            coords = oversample(coords, coords_label, "max")
+
+
+        elif self.dataset_type == "val":
+
+            coords = np.where(self.img_segmentation!=0)
+            coords = np.array(coords)
+            coords = np.rollaxis(coords, 1, 0)
+            
+            coords_label = self.img_segmentation[np.nonzero(self.img_segmentation)]
+
+            coords = oversample(coords, coords_label, "median")
+
+        
+        elif self.dataset_type == "test":            
+            coords_list = []
+
+            overlap_size = int(self.crop_size * self.overlap_rate)
+            stride_size = self.crop_size - overlap_size
+
+            for m in range(self.crop_size//2, self.img_segmentation.shape[0], stride_size):
+
+                for n in range(self.crop_size//2, self.img_segmentation.shape[1], stride_size):
+    
+                    coords_list.append([m,n])
+            
+            coords = np.array(coords_list)
+
+        self.coords = np.array(coords)
+
+
+    def read_window(self, coord, img_path):
+
+
+        pad_width = [[0,0], [0,0]]
+        
+        # check row
+        start_row = coord[0] - self.crop_size // 2
+        if start_row < 0:
+            
+            start_row = 0
+            
+            pad_width[0][0] = abs(coord[0] - self.crop_size // 2)
+            
+
+
+        end_row = coord[0] + self.crop_size // 2
+        if end_row > self.img_segmentation.shape[0]:
+            
+            end_row = self.img_segmentation.shape[0]
+            
+            pad_width[0][1] = abs(coord[0] + self.crop_size//2 - end_row)
+        
+
+        # check column
+        start_column = coord[1] - self.crop_size // 2
+
+        if start_column < 0:
+            
+            start_column = 0
+            
+            pad_width[1][0] = abs(coord[1] - self.crop_size // 2)
+
+
+        end_column = coord[1] + self.crop_size // 2
+        
+        if end_column > self.img_segmentation.shape[1]:
+            
+            end_column = self.img_segmentation.shape[1]
+
+            pad_width[1][1] = abs(coord[1] + (self.crop_size // 2) - end_column)
+
+
+
+        window = rasterio.windows.Window.from_slices(
+            (start_row, end_row), 
+            (start_column, end_column)
+        )
+
+        with rasterio.open(img_path) as src:
+            image_crop = src.read(window = window)
+
+        if len(image_crop.shape) == 3:
+            pad_width = [[0,0]] + pad_width
+
+
+        image_crop = np.pad(
+            image_crop, 
+            pad_width =  pad_width,
+            mode = "constant",
+            constant_values = 0
+        )
+        
+
+        if (image_crop.shape[-1] != self.crop_size) or (image_crop.shape[-2] != self.crop_size):
+            raise ValueError(f"There is a bug relationed to the shape {image_crop.shape}")
+
+        return image_crop
+
+
+    def __len__(self):
+        
+        if (self.samples > len(self.coords)) or (self.samples is None):
+            return len(self.coords)
+        
+        
+        return self.samples
+        
+        
+        
+    def __getitem__(self, idx) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
+        """Get the data from the dataset
+        
+        Parameters
+        ----------
+        idx : int
+            The index of the data to be loaded
+        
+        Returns
+        -------
+        Tuple[torch.Tensor, torch.Tensor, torch.Tensor]
+            The image crop, depth map crop, and label ref crop
+        
+        """
+        current_coord = self.coords[idx]
+        
+        image = self.read_window(current_coord, self.image_path)
+        segmentation = self.read_window(current_coord, self.segmentation_path)
+        distance_map = self.read_window(current_coord, self.distance_map_path)
+
+        return image, distance_map, segmentation
 
     # build data for training
     train_dataset = DatasetFromCoord(
