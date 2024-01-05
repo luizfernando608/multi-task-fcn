a48834ef18b65e7984d9026870c484a8ccef0e65
luizfernando608 <ziulluz@gmail.com>
2024-01-05 15:35:08 -0300

    Raster prediction with lazy loading

--- pred2raster.py
+++ pred2raster.py
@@ -1,69 +1,140 @@
 import os
-from os.path import isfile
+from os.path import isfile, join
 
 import numpy as np
 
-from src.utils import array2raster, check_folder, get_image_metadata, read_yaml
+from src.utils import array2raster, check_folder, fix_relative_paths,get_image_metadata, read_yaml, get_image_shape, read_window, write_window
 
 
 def pred2raster(current_iter_folder, args):
 
-    output_folder = os.path.join(current_iter_folder, 'raster_prediction')
+    output_folder = join(current_iter_folder, 'raster_prediction')
     check_folder(output_folder)
 
-    prediction_file = os.path.join(
+    img_metadata = get_image_metadata(args.train_segmentation_path)
+    img_shape = get_image_shape(args.train_segmentation_path)
+
+    prediction_file = join(
         output_folder,
         f'join_class_itc{args.test_itc}_{np.sum(args.overlap)}.TIF'
-        )
+    )
     
-    prob_file = os.path.join(
+    prob_file = join(
         output_folder,
-        f'join_prob_itc{args.test_itc}_{np.sum(args.overlap)}.TIF')
+        f'join_prob_itc{args.test_itc}_{np.sum(args.overlap)}.TIF'
+    )
+
     
-    
-    depth_file = os.path.join(
+    depth_file = join(
         output_folder,
-        f'depth_itc{args.test_itc}_{np.sum(args.overlap)}.TIF')
+        f'depth_itc{args.test_itc}_{np.sum(args.overlap)}.TIF'
+    )
+    
                
     
-    if not (isfile(prediction_file) and isfile(prob_file) and isfile(depth_file)):
-    
-        for ov in args.overlap:
-            try:
-                prediction_path = os.path.join(current_iter_folder,'prediction',f'prob_map_itc{args.test_itc}_{ov}.npy')
-                prediction_test = np.add(prediction_test, np.load(prediction_path))
-                
-                depth_path = os.path.join(current_iter_folder,'prediction',f'depth_map_itc{args.test_itc}_{ov}.npy')
-                depth_test = np.add(depth_test,np.load(depth_path))
-                
-            except:
-                prediction_path = os.path.join(current_iter_folder,'prediction',f'prob_map_itc{args.test_itc}_{ov}.npy')
-                prediction_test = np.load(prediction_path)
-                
-                depth_path = os.path.join(current_iter_folder,'prediction',f'depth_map_itc{args.test_itc}_{ov}.npy')
-                depth_test = np.load(depth_path)
-                
+    # if not (isfile(prediction_file) and isfile(prob_file) and isfile(depth_file)):
+    if True:
         
-        prediction_test/=3
-        depth_test/=3
+        # create empty tiff files
+        array2raster(
+            prediction_file,
+            np.zeros(img_shape[1:],  dtype = "uint16"),
+            img_metadata,
+            dtype = "uint16"
+        )
 
-        # Get the metadata from segmentation file to save with the output with the same conf
-        RASTER_PATH = args.train_segmentation_path
-        image_metadata = get_image_metadata(RASTER_PATH)
-        
-        # Save the file
-        array2raster(prediction_file, np.argmax(prediction_test, axis = -1), image_metadata, "byte")    
-        array2raster(prob_file, np.amax(prediction_test, axis = -1), image_metadata, "float32")  
-        array2raster(depth_file, depth_test, image_metadata, "float32")   
+        array2raster(
+            prob_file,
+            np.zeros(img_shape[1:],  dtype = "float32"),
+            img_metadata,
+            dtype = "float32"
+        )
 
+
+        array2raster(
+            depth_file,
+            np.zeros(img_shape[1:],  dtype = "float32"),
+            img_metadata,
+            dtype = "float32"
+        )
+
+        PSIZE = 500
+
+        for row in range(0, img_shape[-2], PSIZE):
+            for col in range(0, img_shape[-1], PSIZE):
+                
+                depth_list = []
+                prob_list = []
+
+                for ov in args.overlap:
+                    
+                    prob_path = join(current_iter_folder,'prediction',f'prob_map_itc{args.test_itc}_{ov}.tiff')
+                    depth_path = join(current_iter_folder,'prediction',f'depth_map_itc{args.test_itc}_{ov}.tiff')
+
+                    row_end = min([row + PSIZE, img_shape[-2]])
+
+                    col_end = min([col + PSIZE, img_shape[-1]])
+
+                    bbox = ((row, row_end), (col, col_end))
+                    
+                    # read windows and concat
+                    prob_list.append(
+                        read_window(
+                            prob_path,
+                            bbox,
+                        )
+                    )
+
+                    depth_list.append(
+                        read_window(
+                            depth_path,
+                            bbox,
+                        )
+                    )
+                
+                
+                
+                final_prob = np.mean(np.stack(prob_list, axis = 0), axis = 0)
+
+                write_window(
+                    prob_file,
+                    np.amax(final_prob, axis = 0),
+                    bbox
+                )
+
+
+                write_window(
+                    prediction_file,
+                    np.argmax(final_prob, axis = 0),
+                    bbox
+                )
+
+                final_depth = np.mean(np.stack(depth_list, axis = 0), axis = 0)
+
+                write_window(
+                    depth_file,
+                    final_depth,
+                    bbox = bbox
+                )
+            
+            
+            # calculate the mean
+            
+            # write window with the mean
+            
 
 
 if __name__ == "__main__":
 
     args = read_yaml("args.yaml")
+    
+    fix_relative_paths(args)
+
     # external parameters
-    current_iter_folder = "/home/luiz/multi-task-fcn/MyData/iter_1"
+    current_iter_folder = join(args.data_path, "iter_001")
+
     current_iter = int(current_iter_folder.split("_")[-1])
-    current_model_folder = os.path.join(current_iter_folder, args.model_dir)
+
+    current_model_folder = join(current_iter_folder, args.model_dir)
 
     pred2raster(current_iter_folder, args)
