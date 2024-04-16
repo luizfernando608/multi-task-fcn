ffa0a6ba293a20e82a852a1440680ffcf1c858d5
luizfernando608 <ziulluz@gmail.com>
2024-04-16 10:06:51 -0300

    Function to calculate prediction mean between overlaps

--- pred2raster.py
+++ pred2raster.py
@@ -1,6 +1,7 @@
 import os
-from os.path import isfile
+from os.path import isfile, join
 from logging import getLogger
+from typing import List
 import numpy as np
 
 from src.utils import check_folder
@@ -8,60 +9,76 @@ from src.io_operations import array2raster, get_image_metadata, read_yaml
 
 logger = getLogger("__main__")
 
+
+def delete_prediction_files(current_iter_folder, overlaps):
+    
+    for ov in overlaps:
+        prediction_overlap_path = os.path.join(current_iter_folder, f'prediction_{ov}.npz')
+        os.remove(prediction_overlap_path)
+    
+    
+
+def compute_mean_prediction(data_source:str, overlaps:List[float], current_iter_folder:str):
+    
+    for num, ov in enumerate(overlaps):
+
+        prediction_overlap_path = join(current_iter_folder, f'prediction_{ov}.npz')
+        prediction_ov_data = np.load(prediction_overlap_path)
+        
+        if num == 0:
+            prediction_test = prediction_ov_data[data_source]
+            continue
+
+        else:
+            prediction_test = np.add(prediction_test, prediction_ov_data[data_source])
+        
+        prediction_ov_data.close()
+
+    return prediction_test/len(overlaps)
+        
+  
+
+    
 def pred2raster(current_iter_folder, args):
     
-    logger.info(f"Computing the mean between the {len(args.overlap)} slices")
-
-    output_folder = os.path.join(current_iter_folder, 'raster_prediction')
+    output_folder = join(current_iter_folder, 'raster_prediction')
     check_folder(output_folder)
 
-    prediction_file = os.path.join(
-        output_folder,
-        f'join_class_{np.sum(args.overlap)}.TIF'
-        )
+    prediction_file = join(output_folder,f'join_class_{np.sum(args.overlap)}.TIF')
     
-    prob_file = os.path.join(
-        output_folder,
-        f'join_prob_{np.sum(args.overlap)}.TIF')
+    prob_file = join(output_folder,f'join_prob_{np.sum(args.overlap)}.TIF')
     
-    
-    depth_file = os.path.join(
-        output_folder,
-        f'depth_{np.sum(args.overlap)}.TIF')
+    depth_file = join(output_folder,f'depth_{np.sum(args.overlap)}.TIF')
                
     
-    if not (isfile(prediction_file) and isfile(prob_file) and isfile(depth_file)):
+    if (isfile(prediction_file) and isfile(prob_file) and isfile(depth_file)):
+        return
     
-        for ov in args.overlap:
 
-            prediction_overlap_path = os.path.join(current_iter_folder, f'prediction_{ov}.npz')
-            prediction_ov_data = np.load(prediction_overlap_path)
-            
-            try:
-                prediction_test = np.add(prediction_test, prediction_ov_data["prob_map"])
-                depth_test = np.add(depth_test, prediction_ov_data["depth_map"])
+    logger.info(f"Computing the mean between the {len(args.overlap)} slices")
+    
+    RASTER_PATH = args.train_segmentation_path
+    image_metadata = get_image_metadata(RASTER_PATH)
 
-                
-            except:
-                prediction_test = np.load(prediction_ov_data["prob_map"])
-                depth_test = np.load(prediction_ov_data["depth_map"])
-            
-            prediction_ov_data.close()
-            os.remove(prediction_overlap_path)
-        
-        prediction_test/=len(args.overlap)
-        depth_test/=len(args.overlap)
+    logger.info("Computing the mean of prob_map")
+    prob_map_mean = compute_mean_prediction("prob_map", args.overlap, current_iter_folder)
 
-        # Get the metadata from segmentation file to save with the output with the same conf
-        RASTER_PATH = args.train_segmentation_path
-        image_metadata = get_image_metadata(RASTER_PATH)
-        
-        logger.info("Saving the raster prediction")
-        
-        # Save the file
-        array2raster(prediction_file, np.argmax(prediction_test, axis = -1), image_metadata, "uint8")    
-        array2raster(prob_file, np.amax(prediction_test, axis = -1), image_metadata, "float32")  
-        array2raster(depth_file, depth_test, image_metadata, "float32")   
+    logger.info("Saving prob_map and class_map to raster files")
+    array2raster(prediction_file, np.argmax(prob_map_mean, axis = -1), image_metadata, "uint8")
+    array2raster(prob_file, np.amax(prob_map_mean, axis = -1), image_metadata, "float32")
+    del prob_map_mean
+
+
+    logger.info("Computing the mean of depth_map")
+    depth_map_mean = compute_mean_prediction("depth_map", args.overlap, current_iter_folder)
+    
+    logger.info("Saving prob_map and class_map to raster files")
+    array2raster(depth_file, depth_map_mean, image_metadata, "float32")
+    del depth_map_mean
+
+    delete_prediction_files(current_iter_folder, args.overlap)
+
+    
 
 
 
